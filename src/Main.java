48b50a9bf700f5a7c21a875539d469b4e9e40675
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-03-09 22:49:01 +0530

    used loop for printing table

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Multiplication Table Generator ===");
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        System.out.print("Enter range: ");
+        int range = scanner.nextInt();
+
+        System.out.println("\nMultiplication Table of " + number + ":\n");
+
+        for (int i = 1; i <= range; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+
+        scanner.close();
+    }
 }
