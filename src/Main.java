a08936fa2bc18c15707d0144d809cb52dc469cf2
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-03-10 23:51:17 +0530

    added square table Generator

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,44 @@
 import java.util.Scanner;
+class homepage{
+ void homepage(){
 
+        System.out.println("1) Multiplication Table");
+        System.out.println("2) Square ");
+        System.out.println("3) Cube");
+        System.out.print("Enter Your Choice ");
+    }
+}
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        homepage home = new homepage();
+        home.homepage();
 
-        System.out.println("=== Multiplication Table Generator ===");
-
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-
-        System.out.print("Enter range: ");
-        int range = scanner.nextInt();
-
-        System.out.println("\nMultiplication Table of " + number + ":\n");
-        System.out.println("----------------");
-        for (int i = 1; i <= range; i++) {
-            System.out.println("|"+" "+ i+") "+number + " x " + i + " = " + (number * i)+" |");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+            System.out.println("=== Multiplication Table Generator ===");
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+            System.out.print("Enter range: ");
+            int range = scanner.nextInt();
+            System.out.println("\nMultiplication Table of " + number + ":\n");
+            System.out.println("-------------------");
+            for (int i = 1; i <= range; i++) {
+                System.out.println("|" + " " + i + ") " + number + " x " + i + " = " + (number * i) + " |");
+            }
+            System.out.println("--------------------");
+            break;
+            case 2:
+                System.out.println("==========Square Table Generator=========");
+                System.out.println("Enter the Number");
+                int numberSquare = scanner.nextInt();
+                for (int i = 1;i<2;i++){
+                    System.out.println(numberSquare +" = "+ (numberSquare*numberSquare));
+                }
         }
-        System.out.println("-----------------");
         scanner.close();
     }
 }
