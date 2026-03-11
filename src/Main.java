6de13a79622320093078d14c3cca037c883985d4
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-03-11 23:10:51 +0530

    added while loop for continue process

--- src/Main.java
+++ src/Main.java
@@ -15,54 +15,56 @@ class homepage{
 public class Main {
 
     public static void main(String[] args) {
-
+        while (true){
         Scanner scanner = new Scanner(System.in);
-        homepage home = new homepage();
-        home.homepage();
 
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-            System.out.println("=== Multiplication Table Generator ===");
-            System.out.print("Enter a number: ");
-            int number = scanner.nextInt();
-            System.out.print("Enter range: ");
-            int range = scanner.nextInt();
-            System.out.println("\nMultiplication Table of " + number + ":\n");
-            System.out.println("-------------------");
-            for (int i = 1; i <= range; i++) {
-                System.out.println("|" + " " + i + ") " + number + " x " + i + " = " + (number * i));
+            homepage home = new homepage();
+            home.homepage();
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1:
+                    System.out.println("=== Multiplication Table Generator ===");
+                    System.out.print("Enter a number: ");
+                    int number = scanner.nextInt();
+                    System.out.print("Enter range: ");
+                    int range = scanner.nextInt();
+                    System.out.println("\nMultiplication Table of " + number + ":\n");
+                    System.out.println("-------------------");
+                    for (int i = 1; i <= range; i++) {
+                        System.out.println("|" + " " + i + ") " + number + " x " + i + " = " + (number * i));
+                    }
+                    System.out.println("--------------------");
+                    break;
+                case 2:
+                    System.out.println("==========Square Table Generator=========");
+                    System.out.print("Enter the Number: ");
+                    int numberSquare = scanner.nextInt();
+                    for (int i = 1; i < 2; i++) {
+                        System.out.println(numberSquare + " = " + (numberSquare * numberSquare));
+                    }
+                    break;
+                case 3:
+                    System.out.println("============Cube Table Generator===========");
+                    System.out.print("Enter The Number :");
+                    int cubeNumber = scanner.nextInt();
+                    System.out.println(cubeNumber + " = " + (cubeNumber * cubeNumber * cubeNumber));
+                    break;
+                case 4:
+                    System.out.println("============Square Root Table Generator===========");
+                    System.out.println("Enter The Number");
+                    int squareNumber = scanner.nextInt();
+                    double result = Math.sqrt(squareNumber);
+                    System.out.println(result);
+                    break;
+                case 5:
+                    System.out.println("____________________________________________");
+                    System.out.println("| Thank You For Using Multiplication Table |");
+                    System.out.println("--------------------------------------------");
+                    System.exit(0);
+
             }
-            System.out.println("--------------------");
-            break;
-            case 2:
-                System.out.println("==========Square Table Generator=========");
-                System.out.print("Enter the Number: ");
-                int numberSquare = scanner.nextInt();
-                for (int i = 1;i<2;i++){
-                    System.out.println(numberSquare +" = "+ (numberSquare*numberSquare));
-                }
-                break;
-            case 3:
-                System.out.println("============Cube Table Generator===========");
-                System.out.print("Enter The Number :");
-                int cubeNumber = scanner.nextInt();
-                System.out.println(cubeNumber+" = "+(cubeNumber*cubeNumber*cubeNumber));
-                break;
-            case 4:
-                System.out.println("============Square Root Table Generator===========");
-                System.out.println("Enter The Number");
-                int squareNumber = scanner.nextInt();
-                double result = Math.sqrt(squareNumber);
-                System.out.println(result);
-                break;
-            case 5:
-                System.out.println("____________________________________________");
-                System.out.println("| Thank You For Using Multiplication Table |" );
-                System.out.println("--------------------------------------------");
-                System.exit(0);
 
         }
-        scanner.close();
     }
 }
