4a284621e7bff3f0cedb580341f178787f3ceb07
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-03-11 21:50:25 +0530

    updated the UI of code

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 import java.math.*;
 class homepage{
  void homepage(){
-
-        System.out.println("1) Multiplication Table");
-        System.out.println("2) Square ");
-        System.out.println("3) Cube");
-        System.out.print("Enter Your Choice ");
+        System.out.println("___________________________");
+        System.out.println("| 1) Multiplication Table |");
+        System.out.println("| 2) Square               |");
+        System.out.println("| 3) Cube                 |");
+        System.out.println("---------------------------");
+        System.out.print("Enter Your Choice: ");
     }
 }
 public class Main {
@@ -28,13 +29,13 @@ public class Main {
             System.out.println("\nMultiplication Table of " + number + ":\n");
             System.out.println("-------------------");
             for (int i = 1; i <= range; i++) {
-                System.out.println("|" + " " + i + ") " + number + " x " + i + " = " + (number * i) + " |");
+                System.out.println("|" + " " + i + ") " + number + " x " + i + " = " + (number * i));
             }
             System.out.println("--------------------");
             break;
             case 2:
                 System.out.println("==========Square Table Generator=========");
-                System.out.println("Enter the Number");
+                System.out.print("Enter the Number: ");
                 int numberSquare = scanner.nextInt();
                 for (int i = 1;i<2;i++){
                     System.out.println(numberSquare +" = "+ (numberSquare*numberSquare));
@@ -42,7 +43,7 @@ public class Main {
                 break;
             case 3:
                 System.out.println("============Cube Table Generator===========");
-                System.out.println("Enter The Number");
+                System.out.print("Enter The Number :");
                 int cubeNumber = scanner.nextInt();
                 System.out.println(cubeNumber+" = "+(cubeNumber*cubeNumber*cubeNumber));
 
